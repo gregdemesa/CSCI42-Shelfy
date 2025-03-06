4327bee074f64740c71538e523fb09ba154dcf50
Zia Garcia <maigelazia.garcia@gmail.com>
2025-03-06 14:28:34 +0800

    removed media app after integrating into main

--- media/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MediaAPI
-
-urlpatterns = [
-    path("search/", MediaAPI.as_view(), name="media_search"),
-    path("<str:media_type>/<str:external_id>/", MediaAPI.as_view(), name="media_detail"),
-]
