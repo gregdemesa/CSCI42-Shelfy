738d6cece7366cfb616677df6ecdb965461cedfc
giulianabautista <157723350+giulianabautista@users.noreply.github.com>
2025-03-13 12:33:13 +0800

    chart placeholders

--- charts/urls.py
+++ charts/urls.py
@@ -5,7 +5,7 @@ app_name = 'statistics'
 
 urlpatterns = [ 
     # path('admin/', admin.site.urls), 
-    path('statistics', HomeView.as_view(), name="statistics_home"), 
+    path('statistics/', HomeView.as_view(), name="statistics_home"), 
     # path('test-api', views.get_data), 
     # path('statistics/chart', ChartData.as_view(), name="statistics"), 
 ] 
