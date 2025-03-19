1a617ec98e0c1a12a5006751d5a978455ad8bff7
Zia Garcia <maigelazia.garcia@gmail.com>
2025-03-19 19:26:24 +0800

    cleaned up code and removed unnecessary template

--- shelfy/settings.py
+++ shelfy/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_management',
     'shelfy',
+    'user_management',
     'user_library',
 ]
 
