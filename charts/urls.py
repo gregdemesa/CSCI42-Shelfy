16537f9d08ed6b84a7701b9bcc23f06b6b1c91ac
giulianabautista <157723350+giulianabautista@users.noreply.github.com>
2025-04-06 16:24:05 +0800

    fixed views.py and added books and games statistics

--- charts/urls.py
+++ charts/urls.py
@@ -1,16 +1,26 @@
 from django.urls import path
 from charts import views
-from .views import AverageRatingView, MovieCountView
+from .views import AllView, MovieView, BookView, GameView
 
 app_name = 'charts'
 
 urlpatterns = [
-    path('all-statistics', AverageRatingView.as_view(), name='charts'),
+    path('all-statistics', AllView.as_view(), name='charts'),
     path('all-count', views.all_count, name='all_count'),
     path('all-ratings', views.all_ratings, name='all_ratings'),
-    path('movies', MovieCountView.as_view(), name='movie'),
+    path('movies', MovieView.as_view(), name='movie'),
     path('movies-chart', views.movies_chart, name='movies_charts'),
     path('movies-rating', views.movies_ratings, name='movies_ratings'),
     path('movies-director', views.movies_directors, name='movies_directors'),
-    path('movies-release', views.movies_release_years, name='movies_release')
+    path('movies-release', views.movies_release_years, name='movies_release'),
+    path('books', BookView.as_view(), name='book'),
+    path('books-chart', views.books_chart, name='books_charts'),
+    path('books-rating', views.books_ratings, name='books_ratings'),
+    path('books-author', views.books_authors, name='books_authors'),
+    path('books-release', views.books_release_years, name='books_release'),
+    path('games', GameView.as_view(), name='game'),
+    path('games-chart', views.games_chart, name='games_charts'),
+    path('games-rating', views.games_ratings, name='games_ratings'),
+    path('games-studio', views.games_studio, name='games_studio'),
+    path('games-release', views.games_release_years, name='games_release'),
 ]
--- charts/views.py
+++ charts/views.py
@@ -1,45 +1,121 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.db.models import Max, Value, CharField, Avg
-from django.db.models.functions import Concat
+# from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+# from django.db.models import Max, Value, CharField, Avg
+# from django.db.models.functions import Concat
 
 from user_library.models import UserLibraryItem
 from django.http import JsonResponse
 from collections import defaultdict
 
+'''
+Helper functions set-up
+'''
 
-class AverageRatingView(ListView):
+def sort_data(rating_data):
+    return sorted(rating_data.items())
+
+def get_average_rating(media_type=None):
+    """
+    Helper function to calculate the average rating for a given media type.
+    If no media_type is given, it calculates the average across all media types.
+    """
+    if media_type:
+        ratings = UserLibraryItem.objects.filter(media__media_type__iexact=media_type).values_list("rating", flat=True)
+    else:
+        ratings = UserLibraryItem.objects.values_list("rating", flat=True)
+    
+    ratings = list(filter(None, ratings))
+    return round(sum(ratings) / len(ratings), 2) if ratings else 0
+
+def count_ratings(ratings_query):
+    """
+    Helper function to count ratings from a queryset.
+    """
+    ratings_count = defaultdict(int)
+    ratings = list(filter(None, ratings_query))
+
+    for rate in ratings:
+        ratings_count[rate] += 1
+
+    # makes sure that all ratings have a value
+    for rating in range(1, 6):
+        if rating not in ratings_count:
+            ratings_count[rating] = 0
+
+    return ratings_count
+
+def count_release_years(ratings_query):
+    """
+    Helper function to count release years from a queryset.
+    """
+    years_count = defaultdict(int)
+    years = list(filter(None, ratings_query))
+
+    for year in years:
+        years_count[year.media.release_year] += 1
+
+    return years_count
+
+
+''' Classes '''
+class AllView(ListView):
     model = UserLibraryItem
     template_name = "chartjs/index.html"
 
-    def get_context_data(self, **kwargs): # from Django Book/ Lecture
-        context = super().get_context_data(**kwargs) #overides the other get_context
-        # Average star ratings across all media
-        rating = UserLibraryItem.objects.values_list("rating", flat=True)
-        rating = list(filter(None, rating))
-        # Gets All Movie ratings
-        movie_rating = UserLibraryItem.objects.filter(media__media_type__iexact="movie").values_list("rating", flat=True)
-        movie_rating = list(filter(None, movie_rating))
-        # Gets All Book ratings
-        book_rating = UserLibraryItem.objects.filter(media__media_type__iexact="book").values_list("rating", flat=True)
-        book_rating = list(filter(None, book_rating))
-        # Gets All Game ratings
-        game_rating = UserLibraryItem.objects.filter(media__media_type__iexact="game").values_list("rating", flat=True)
-        game_rating = list(filter(None, game_rating))
-        # Gets Average star ratings of each component, rounded up by 2 decimals
-        context['rating'] = round(sum(rating)/len(rating),2)
-        context['movie_rating'] = round(sum(movie_rating)/len(movie_rating),2)
-        context['book_rating'] = round(sum(book_rating)/len(book_rating),2)
-        context['game_rating'] = round(sum(game_rating)/len(game_rating),2)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Get average ratings for each media type
+        context['rating'] = get_average_rating()
+        context['movie_rating'] = get_average_rating('movie')
+        context['book_rating'] = get_average_rating('book')
+        context['game_rating'] = get_average_rating('game')
+
+        return context
+    
+
+class MovieView(ListView):
+    model = UserLibraryItem
+    template_name = "chartjs/movies.html"
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
         movie_count = len(UserLibraryItem.objects.filter(media__media_type__iexact="movie"))
         context['movie_count'] = movie_count
+        context['movie_rating'] = get_average_rating('movie')
         
         return context
+    
 
+class BookView(ListView):
+    model = UserLibraryItem
+    template_name = "chartjs/books.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) 
+        book_count = len(UserLibraryItem.objects.filter(media__media_type__iexact="book"))
+        context['book_count'] = book_count
+        context['book_rating'] = get_average_rating('book')
+        
+        return context
+    
+class GameView(ListView):
+    model = UserLibraryItem
+    template_name = "chartjs/games.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        game_count = len(UserLibraryItem.objects.filter(media__media_type__iexact="game"))
+        context['game_count'] = game_count
+        context['game_rating'] = get_average_rating('game')
+        
+        return context
+    
+# Count of All Media Types
 def all_count(request):
     movie_count = len(UserLibraryItem.objects.filter(media__media_type__iexact="movie"))
     book_count = len(UserLibraryItem.objects.filter(media__media_type__iexact="book"))
@@ -51,79 +127,34 @@ def all_count(request):
         'game_count': game_count
     }) 
 
+''' Functions for Ratings '''
+# Count of all ratings across all media types
 def all_ratings(request):
-    ratings_count = defaultdict(int)
+    # Get all ratings
+    all_ratings = UserLibraryItem.objects.values_list("rating", flat=True)
 
-    all_rating = UserLibraryItem.objects.values_list("rating", flat=True)
-    all_rating = list(filter(None, all_rating))
-    # Gets All Movie ratings
-    movie_ratings_count = defaultdict(int)
-    movie_rating = UserLibraryItem.objects.filter(media__media_type__iexact="movie").values_list("rating", flat=True)
-    movie_rating = list(filter(None, movie_rating))
-    # Gets All Book ratings
-    book_ratings_count = defaultdict(int)
-    book_rating = UserLibraryItem.objects.filter(media__media_type__iexact="book").values_list("rating", flat=True)
-    book_rating = list(filter(None, book_rating))
-    # Gets All Game ratings
-    game_ratings_count = defaultdict(int)
-    game_rating = UserLibraryItem.objects.filter(media__media_type__iexact="game").values_list("rating", flat=True)
-    game_rating = list(filter(None, game_rating))
+    # Get ratings per media type
+    movie_ratings_count = count_ratings(
+        UserLibraryItem.objects.filter(media__media_type__iexact="movie").values_list("rating", flat=True)
+    )
+    book_ratings_count = count_ratings(
+        UserLibraryItem.objects.filter(media__media_type__iexact="book").values_list("rating", flat=True)
+    )
+    game_ratings_count = count_ratings(
+        UserLibraryItem.objects.filter(media__media_type__iexact="game").values_list("rating", flat=True)
+    )
 
-    for rate in all_rating:
-        ratings_count[rate]+=1
-    for rate in movie_rating:
-        movie_ratings_count[rate]+=1
-    for rate in book_rating:
-        book_ratings_count[rate]+=1
-    for rate in game_rating:
-        game_ratings_count[rate]+=1
+    # Get the overall ratings count
+    ratings_count = count_ratings(all_ratings)
 
-    #for rate in all_rating:
-        # if the a game has been rated, rate, add +1 to game_ratings_count[rate]
-        # else, initialize as 0
+    # Prepare the data for the response
+    ratings_all_data = sort_data(ratings_count)
+    movies_count_data = sort_data(movie_ratings_count)
+    books_count_data = sort_data(book_ratings_count)
+    games_count_data = sort_data(game_ratings_count)
 
-    for rate in all_rating:
-        if not movie_ratings_count[rate]:
-            movie_ratings_count[rate] == 0
-        if not book_ratings_count[rate]:
-            book_ratings_count[rate] == 0
-        if not game_ratings_count[rate]:
-            game_ratings_count[rate] == 0
-        
-        
-
-
-    rating = list(ratings_count.keys())
-    all_counts = list(ratings_count.values())
-
-    ratings_all_data = list(zip(rating,all_counts))
-    
-    ratings_all_data.sort(key=lambda x: x[0])
-
+    # Unzip the sorted data into separate lists for ratings and counts
     sorted_rating, sorted_rating_counts = zip(*ratings_all_data)
-
-    # return JsonResponse(data={
-    #     'rating': sorted_rating,
-    #     'movie': sorted_rating_counts
-    # })
-
-    movies = list(movie_ratings_count.keys())
-    movies_count = list(movie_ratings_count.values())
-
-    books = list(book_ratings_count.keys())
-    books_count = list(book_ratings_count.values())
-
-    games = list(game_ratings_count.keys())
-    games_count = list(game_ratings_count.values())
-
-    movies_count_data = list(zip(movies, movies_count))
-    books_count_data = list(zip(books, books_count))
-    games_count_data = list(zip(games, games_count))
-
-    movies_count_data.sort(key=lambda x: x[0])
-    books_count_data.sort(key=lambda x: x[0])
-    games_count_data.sort(key=lambda x: x[0])
-
     sorted_movies, sorted_movies_count = zip(*movies_count_data)
     sorted_books, sorted_books_count = zip(*books_count_data)
     sorted_games, sorted_games_count = zip(*games_count_data)
@@ -137,22 +168,52 @@ def all_ratings(request):
         'game_rating': sorted_games,
         'game': sorted_games_count
     })
-
-class MovieCountView(ListView):
-    model = UserLibraryItem
-    template_name = "chartjs/movies.html"
-
-    def get_context_data(self, **kwargs): # from Django Book/ Lecture
-        context = super().get_context_data(**kwargs) #overides the other get_context
-        movie_count = len(UserLibraryItem.objects.filter(media__media_type__iexact="movie"))
-        movie_rating = UserLibraryItem.objects.filter(media__media_type__iexact="movie").values_list("rating", flat=True)
-        movie_rating = list(filter(None, movie_rating))
-        context['movie_count'] = movie_count
-        context['movie_rating'] = round(sum(movie_rating)/len(movie_rating),2)
-        
-        return context
     
+# count of movie ratings
+def movies_ratings(request):
+    movie_ratings_count = count_ratings(
+        UserLibraryItem.objects.filter(media__media_type__iexact="movie").values_list("rating", flat=True)
+    )
+    
+    movies_count_data = sort_data(movie_ratings_count)
 
+    sorted_rating, sorted_rating_counts = zip(*movies_count_data)
+
+    return JsonResponse(data={
+        'rating': sorted_rating,
+        'movie': sorted_rating_counts
+    })
+
+def books_ratings(request):
+    book_ratings_count = count_ratings(
+        UserLibraryItem.objects.filter(media__media_type__iexact="book").values_list("rating", flat=True)
+    )
+    
+    books_count_data = sort_data(book_ratings_count)
+
+    sorted_rating, sorted_rating_counts = zip(*books_count_data)
+
+    return JsonResponse(data={
+        'rating': sorted_rating,
+        'book': sorted_rating_counts
+    })
+
+def games_ratings(request):
+    ratings_count = count_ratings(
+        UserLibraryItem.objects.filter(media__media_type__iexact="game").values_list("rating", flat=True)
+    )
+    
+    count_data = sort_data(ratings_count)
+
+    sorted_rating, sorted_rating_counts = zip(*count_data)
+
+    return JsonResponse(data={
+        'rating': sorted_rating,
+        'game': sorted_rating_counts
+    })
+
+''' Functions for genres '''
+# count of movie genres
 def movies_chart(request):
     # Storing genre and corresponding number of movies with that genre
     genre_count = defaultdict(int)
@@ -169,7 +230,6 @@ def movies_chart(request):
         for genre in genres:
             genre_count[genre.strip()] += 1  # Cleans whitespace between genres
     
-
     # Prepare the genre and movie count lists
     genres = list(genre_count.keys())
     movie_counts = list(genre_count.values())
@@ -179,36 +239,116 @@ def movies_chart(request):
     
     # Sort the list by movie counts in descending order
     genre_movie_data.sort(key=lambda x: x[1], reverse=True)
+    top_10_genres_movies = genre_movie_data
 
     # Unzip the sorted data into separate lists again
-    sorted_genres, sorted_movie_counts = zip(*genre_movie_data)
+    sorted_genres, sorted_movie_counts = zip(*top_10_genres_movies)
 
     return JsonResponse(data={
         'genre': list(sorted_genres),
         'movie': list(sorted_movie_counts),
     })
 
-def movies_ratings(request):
-    ratings_count = defaultdict(int)
-
-    movie_rating = UserLibraryItem.objects.filter(media__media_type__iexact="movie").values_list("rating", flat=True)
-
-    for rate in movie_rating:
-        ratings_count[rate]+=1
+def books_chart(request):
+    genre_count = defaultdict(int)
     
-    rating = list(ratings_count.keys())
-    movie_counts = list(ratings_count.values())
-
-    ratings_movie_data = list(zip(rating, movie_counts))
+    book_genre = UserLibraryItem.objects.filter(media__media_type__iexact="book")
     
-    ratings_movie_data.sort(key=lambda x: x[0])
+    for entry in book_genre:
+        genres = entry.media.genre.split("/")
+        
+        for genre in genres:
+            genre_count[genre.strip()] += 1 
+    
+    genres = list(genre_count.keys())
+    book_counts = list(genre_count.values())
+    genre_book_data = list(zip(genres, book_counts))
+    
+    genre_book_data.sort(key=lambda x: x[1], reverse=True)
+    top_10_genres_books = genre_book_data[:10]
 
-    sorted_rating, sorted_rating_counts = zip(*ratings_movie_data)
+    sorted_genres, sorted_book_counts = zip(*top_10_genres_books)
 
     return JsonResponse(data={
-        'rating': sorted_rating,
+        'genre': list(sorted_genres),
+        'book': list(sorted_book_counts),
+    })
+
+def games_chart(request):
+    genre_count = defaultdict(int)
+    
+    game_genre = UserLibraryItem.objects.filter(media__media_type__iexact="game")
+    
+    for entry in game_genre:
+        genres = entry.media.genre.split(",")
+        
+        for genre in genres:
+            genre_count[genre.strip()] += 1 
+    
+    genres = list(genre_count.keys())
+    game_counts = list(genre_count.values())
+    genre_game_data = list(zip(genres, game_counts))
+    
+    genre_game_data.sort(key=lambda x: x[1], reverse=True)
+    top_10_genres_games = genre_game_data[:10]
+
+    sorted_genres, sorted_game_counts = zip(*top_10_genres_games)
+
+    return JsonResponse(data={
+        'genre': list(sorted_genres),
+        'game': list(sorted_game_counts),
+    })
+
+'''
+Functions for Release Years
+'''
+
+def movies_release_years(request):
+    movie_release_years_count = count_release_years(
+        UserLibraryItem.objects.filter(media__media_type__iexact="movie")
+    )
+    
+    movies_count_data = sort_data(movie_release_years_count)
+
+    sorted_years, sorted_rating_counts = zip(*movies_count_data)
+
+    return JsonResponse(data={
+        'release_year': sorted_years,
         'movie': sorted_rating_counts
     })
+
+def books_release_years(request):
+    book_release_years_count = count_release_years(
+        UserLibraryItem.objects.filter(media__media_type__iexact="book")
+    )
+    
+    books_count_data = sort_data(book_release_years_count)
+
+    sorted_years, sorted_rating_counts = zip(*books_count_data)
+
+    return JsonResponse(data={
+        'release_year': sorted_years,
+        'book': sorted_rating_counts
+    })
+
+def games_release_years(request):
+    release_years_count = count_release_years(
+        UserLibraryItem.objects.filter(media__media_type__iexact="game")
+    )
+    
+    count_data = sort_data(release_years_count)
+
+    sorted_years, sorted_rating_counts = zip(*count_data)
+
+    return JsonResponse(data={
+        'release_year': sorted_years,
+        'game': sorted_rating_counts
+    })
+
+'''
+functions that are not final 
+(Directors/Authors/Publishers)
+'''
 
 def movies_directors(request):
     director_count = defaultdict(int)
@@ -242,24 +382,66 @@ def movies_directors(request):
         'movie': list(sorted_movie_counts),
     })
 
-def movies_release_years(request):
-    release_year_count = defaultdict(int)
+def books_authors(request):
+    director_count = defaultdict(int)
     
-    release_years = UserLibraryItem.objects.filter(media__media_type__iexact="movie")
+    movie_directors = UserLibraryItem.objects.filter(media__media_type__iexact="book")
     
-    for entry in release_years:
-        release_year_count[entry.media.release_year] += 1
-    
-    years = list(release_year_count.keys())
-    years_count = list(release_year_count.values())
+    for entry in movie_directors:
+        # Splitting the list of genres if a movie has more than one genre
+        directors = entry.media.author.split(',')
+        
+        # Increment the count for each genre
+        for director in directors:
+            director_count[director.strip()] += 1  # Cleans whitespace between genres
+        # director_count[entry.media.author] += 1
 
-    release_year_movie_data = list(zip(years, years_count))
-    
-    release_year_movie_data.sort(key=lambda x: x[0])
+    # Prepare the genre and movie count lists
+    directors = list(director_count.keys())
+    director_counts = list(director_count.values())
 
-    sorted_years, sorted_years_counts = zip(*release_year_movie_data)
+    # Combine genre and movie count into a list of tuples
+    director_movie_data = list(zip(directors, director_counts))
+    
+    # Sort the list by movie counts in descending order
+    director_movie_data.sort(key=lambda x: x[1], reverse=True)
+
+    # Unzip the sorted data into separate lists again
+    sorted_directors, sorted_movie_counts = zip(*director_movie_data)
 
     return JsonResponse(data={
-        'release_year': list(sorted_years),
-        'movie': list(sorted_years_counts),
+        'author': list(sorted_directors),
+        'book': list(sorted_movie_counts),
+    })
+
+def games_studio(request):
+    director_count = defaultdict(int)
+    
+    movie_directors = UserLibraryItem.objects.filter(media__media_type__iexact="game")
+    
+    for entry in movie_directors:
+        # # Splitting the list of genres if a movie has more than one genre
+        # directors = entry.media.director.split(",")
+        
+        # # Increment the count for each genre
+        # for director in directors:
+        #     director_count[director.strip()] += 1  # Cleans whitespace between genres
+        director_count[entry.media.studio] += 1
+
+    # Prepare the genre and movie count lists
+    directors = list(director_count.keys())
+    director_counts = list(director_count.values())
+
+    # Combine genre and movie count into a list of tuples
+    director_movie_data = list(zip(directors, director_counts))
+    
+    # Sort the list by movie counts in descending order
+    director_movie_data.sort(key=lambda x: x[1], reverse=True)
+
+    # Unzip the sorted data into separate lists again
+    sorted_directors, sorted_movie_counts = zip(*director_movie_data)
+
+    return JsonResponse(data={
+        'studio': list(sorted_directors),
+        'game': list(sorted_movie_counts),
     })
